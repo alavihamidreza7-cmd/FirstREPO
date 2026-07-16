193fb47bbf67d9badba2d7d5c0307c04af8407f7
alavihamidreza7@gmail.com <HAMIDMILAD@#386>
2026-07-16 15:40:46 +0330

    So Bad

--- Third.c
+++ Third.c
@@ -7,23 +7,7 @@
 
 
 int main(){
-    int **x=(int **)malloc(2*sizeof(int *));
-    for(int i=0;i<5;++i){
-        x[i]=(int *)malloc(2*sizeof(int));
-    }
-    for(int i=0;i<2;++i){
-        for(int j=0;j<2;++j){
-            x[i][j]=12;
-    }
-    }
-    for(int i=0;i<2;++i){
-        for(int j=0;j<2;++j){
-            printf("%d\n",x[i][j]);
-    }
-    }
-
-    for(int i=0;i<2;++i){
-        free(x[i]);
+    free(x[i]);
     }
     free(x);
     int *p;
